06a7b3cff8b7e7bd8e77bb74bbb955e431ed7a81
Erikas <erikmnkl@gmail.com>
2022-02-26 14:56:50 +0200

    update docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -6,4 +6,4 @@ VERSION=0.0.1
 PLATFORM=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7,linux/arm/v6
 
 docker buildx create --use --name multiarch --platform ${PLATFORM} || true
-docker buildx build --push --platform ${PLATFORM} --tag erikmnkl/stoppropaganda:${VERSION} .
+docker buildx build --push --platform ${PLATFORM} --tag erikmnkl/stoppropaganda:${VERSION} --tag erikmnkl/stoppropaganda:latest .

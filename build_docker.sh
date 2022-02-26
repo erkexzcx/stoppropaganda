fe57147c70fe1b0cc9873f15959c89c292788c26
Erikas <erikmnkl@gmail.com>
2022-02-26 21:00:22 +0200

    install emulators before building in docker

--- build_docker.sh
+++ build_docker.sh
@@ -5,5 +5,6 @@
 VERSION=0.0.4
 PLATFORM=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7,linux/arm/v6
 
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --name multiarch --platform ${PLATFORM} || true
 docker buildx build --push --platform ${PLATFORM} --tag erikmnkl/stoppropaganda:${VERSION} --tag erikmnkl/stoppropaganda:latest .

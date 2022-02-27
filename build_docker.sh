dea26c6364fe29603cb8c006dd0cb688d2c3898d
Erikas <erikmnkl@gmail.com>
2022-02-27 16:59:29 +0200

    new version

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,7 @@
 
 # Before using this script, perform "docker login" :)
 
-VERSION=0.0.13
+VERSION=0.0.14
 PLATFORM=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7,linux/arm/v6
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes

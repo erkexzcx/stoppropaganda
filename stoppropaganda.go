a9422cf2890a724e0ae350adfa7f2a14cc66c410
valuievruslan <51120432+hellhoundSE@users.noreply.github.com>
2022-02-28 09:31:12 +0100

    add moscow exchange

--- stoppropaganda.go
+++ stoppropaganda.go
@@ -94,6 +94,7 @@ var links = map[string]struct{}{
 	"https://api.sberbank.ru/prod/tokens/v2":           {},
 	"https://api.sberbank.ru/prod/tokens/v2/oauth":     {},
 	"https://api.sberbank.ru/prod/tokens/v2/oidc":      {},
+	"https://www.moex.com": {},
 
 	//The state
 	"https://gosuslugi.ru":      {},

653389ed319a22a27cdfc65ab11ea271f2422316
Erikas Rudinskas <erikmnkl@gmail.com>
2022-03-04 12:15:37 +0200

    optimise code

--- internal/stoppropaganda/resolvefix/resolvefix.go
+++ internal/stoppropaganda/resolvefix/resolvefix.go
@@ -6,22 +6,18 @@ import (
 )
 
 func CheckNonPublicTCPEndpoints(addrs []net.TCPAddr) error {
-	ips := make([]net.IP, len(addrs))
-	for i, addr := range addrs {
-		ips[i] = addr.IP
+	ips := make([]net.IP, 0, len(addrs))
+	for _, addr := range addrs {
+		ips = append(ips, addr.IP)
 	}
 	return CheckNonPublicIPs(ips)
 }
 
 func CheckNonPublicIPs(ips []net.IP) error {
 	for _, ip := range ips {
-		if IsNonPublic(ip) {
+		if ip.IsPrivate() || ip.IsLoopback() || ip.IsUnspecified() {
 			return errors.New("Non public IP detected: " + ip.String())
 		}
 	}
 	return nil
-}
-
-func IsNonPublic(ip net.IP) bool {
-	return ip.IsPrivate() || ip.IsLoopback() || ip.IsUnspecified()
 }

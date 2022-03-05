3f2162a9cfc4e5a6c1f24fc864222860159c6d3d
Erikas Rudinskas <erikmnkl@gmail.com>
2022-03-05 13:47:52 +0200

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.37
+VERSION=0.0.38
 BINARY_NAME=stoppropaganda
 
 # Remove old binaries (if any)
--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,7 @@
 
 # Before using this script, perform "docker login" :)
 
-VERSION=0.0.37
+VERSION=0.0.38
 PLATFORM=linux/amd64,linux/arm64,linux/ppc64le,linux/386,linux/arm/v7
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes

d087e212f6ab44e634cefa8fcbb9d63428690b2e
Erikas <erikmnkl@gmail.com>
2022-03-13 11:04:51 +0200

    enable by default

--- internal/stoppropaganda/stoppropaganda.go
+++ internal/stoppropaganda/stoppropaganda.go
@@ -33,7 +33,7 @@ var (
 	flagProxyBypass     = fs.String("proxybypass", "", "list of comma separated IP addresses, CIDR ranges, zones (*.example.com) or a hostnames (e.g. localhost) that needs to bypass used proxy")
 	flagAlgorithm       = fs.String("algorithm", "fair", "allowed algorithms are 'fair' and 'rr' (refer to README.md documentation)")
 	flagMaxProcs        = fs.Int("maxprocs", 1, "amount of threads used by Golang (runtime.GOMAXPROCS)")
-	flagRandomQuery     = fs.Bool("randomquery", false, "append random query to URLs to disrupt caching mechanisms")
+	flagRandomQuery     = fs.Bool("randomquery", true, "append random query to URLs to disrupt caching mechanisms")
 )
 
 func Start() {

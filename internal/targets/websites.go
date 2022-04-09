2c6b252d8befe2778d20fd9ddb329e95fcd60006
Flux State <f1ux.state@pm.me>
2022-04-09 22:18:36 +0200

    fix

--- internal/targets/websites.go
+++ internal/targets/websites.go
@@ -549,5 +549,5 @@ var TargetWebsites = map[string]struct{}{
 	"https://www.amediateka.ru": {},
 	"https://www.okko.tv": {},
 	"https://www.ontvtime.ru": {},
-	"https://wink.ru",
+	"https://wink.ru": {},
 }
